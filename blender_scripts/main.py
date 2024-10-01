dbe6fc7243af805ceb34fade4a55200744c039e8
Kasper Svane <svane.kasper20@gmail.com>
2024-10-01 02:27:24 +0200

    task/terrain
    Working on generating terrain map

--- blender_scripts/main.py
+++ blender_scripts/main.py
@@ -1,90 +1,16 @@
-import time
-
 import bpy
-from mathutils import Vector
-from mathutils.noise import fractal
 
 import logging
-import matplotlib.pyplot as plt
-
-import numpy as np
-from PIL import Image
-
 
 from consts import Constants
 from rendering.rendering import setup_rendering
 from configs.configuration import Configuration, save_configuration, load_configuration
-from terrain import generate_segmentation
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 logger = logging.getLogger(__name__)
-
-def generate_segmentation_map(
-        world_size: float = 100.0,
-        n_octaves: tuple[int, int] = (3, 4),
-        H: tuple[float, float] = (0.4, 0.5),
-        lacunarity: tuple[float, float] = (1.1, 1.2),
-        img_size: int = 2048,
-        band: int = 48,
-        noise_basis: str = "PERLIN_ORIGINAL"
-):
-    timeit = time.time()
-
-    grid = np.linspace(-world_size / 2, world_size / 2, 1000, endpoint=True)
-
-    np.random.seed(42)
-
-    restart = np.random.randint(world_size // 3, world_size // 2)
-    n_octaves = int(np.random.randint(*n_octaves))
-    H = np.random.uniform(*H)
-    lacunarity = np.random.uniform(*lacunarity)
-    offset = np.random.randint(0, world_size // 2)
-
-    d_map = []
-    for x in grid:
-        d_row = []
-        for y in grid:
-            z = fractal(
-                Vector((x / restart + offset, y / restart + offset, 0)),
-                H,
-                lacunarity,
-                n_octaves,
-                noise_basis=noise_basis,
-            )
-            d_row.append(z)
-        d_map.append(d_row)
-    d_map = np.array(d_map)
-
-    d_map = Image.fromarray(d_map).resize((img_size,) * 2, Image.Resampling.BILINEAR)
-    d_map = np.array(d_map)
-
-    d_min = np.min(d_map)
-    d_max = np.max(d_map)
-
-    t_gen = (d_map - d_min) / (d_max - d_min) * 255
-
-    grass = (t_gen < (128 + band)) * (t_gen > (128 - band))
-    grass = grass.astype(np.uint8) * 255
-
-    not_grass = t_gen >= (128 + band)
-    not_grass = not_grass.astype(np.uint8) * 255
-
-    beds = t_gen <= (128 - band)
-    beds = beds.astype(np.uint8) * 255
-
-    seg_map = np.zeros((img_size, img_size, 3), dtype=np.uint8)
-    seg_map[..., 0] = not_grass
-    seg_map[..., 1] = grass
-    seg_map[..., 2] = beds
-
-    t_gen /= 255
-
-    logger.info(f"Old method Time taken: {time.time() - timeit:.2f} seconds")
-
-    return t_gen, seg_map, (grass, not_grass, beds)
 
 
 def clear_cube() -> None:
@@ -104,21 +30,11 @@ def setup() -> None:
         config = save_configuration(configuration=Configuration().model_dump(), path=Constants.Directory.CONFIG_PATH)
 
     configuration = Configuration(**config)
-    terrain_config = configuration.terrain_configuration
 
     setup_rendering(
         render_configuration=configuration.render_configuration,
         camera_configuration=configuration.camera_configuration,
     )
-
-    new = generate_segmentation(world_size=int(terrain_config.world_size), image_size=terrain_config.image_size)
-    old = generate_segmentation_map(world_size=int(terrain_config.world_size), img_size=terrain_config.image_size)
-
-    plt.hist(old[0].flatten(), bins=50, alpha=0.7, label='Old Method')
-    plt.hist(new[0].flatten(), bins=50, alpha=0.5, label='New Method')
-    plt.legend(loc='upper right')
-    plt.title('Distribution of Height Map Values')
-    plt.show()
 
 
 def main() -> None:
--- blender_scripts/terrain.py
+++ blender_scripts/terrain.py
@@ -1,13 +1,13 @@
-import time
-
 from mathutils import Vector
 from mathutils.noise import fractal
+
+import time
+
+from PIL import Image
 
 import numpy as np
 from typing import Tuple
 import logging
-
-from numpy import ndarray
 
 from consts import Constants
 
@@ -45,10 +45,8 @@ def generate_segmentation(
         masks: Tuple of binary masks (grass, non-grass, beds).
     """
 
-    timeit = time.time()
-
     # Step 1: Generate normalized height map
-    height_map, seg_map, (grass, not_grass, beds) = _generate_height_map(
+    normalized_height_map = _generate_height_map(
         world_size=world_size,
         image_size=image_size,
         num_octaves=num_octaves,
@@ -57,20 +55,23 @@ def generate_segmentation(
         noise_basis=noise_basis
     )
 
-    logger.info(f"New Method time taken: {time.time() - timeit:.2f} seconds")
+    # Step 2: Generate segmentation map
+    height_map, seg_map, (grass, not_grass, beds) = _generate_segmentation_map(
+        height_map=normalized_height_map,
+        band=band
+    )
 
     return height_map, seg_map, (grass, not_grass, beds)
 
 
 def _generate_height_map(
-        world_size: int = int(Constants.Default.WORLD_SIZE),
+        world_size: float = Constants.Default.WORLD_SIZE,
         num_octaves: Tuple[int, int] = (3, 4),
         H: Tuple[float, float] = (0.4, 0.5),
         lacunarity: Tuple[float, float] = (1.1, 1.2),
         image_size: int = Constants.Default.IMAGE_SIZE,
-        band: int = 48,
         noise_basis: str = Constants.Default.NOISE_BASIS
-) -> tuple[ndarray | ndarray, ndarray | ndarray, tuple[ndarray | ndarray, ndarray | ndarray, ndarray | ndarray]]:
+) -> np.ndarray:
     """
     Generates a normalized height map based on fractal noise.
 
@@ -83,21 +84,19 @@ def _generate_height_map(
         noise_basis: Type of noise basis (e.g., "PERLIN_ORIGINAL").
 
     Returns:
-        A height map (2D array) normalized to [0, 1] representing terrain heights.
+        A height map (2D array) normalized to [0, 255] representing terrain heights.
     """
-    np.random.seed(42)
-
     # Generate a grid of points
-    grid = np.linspace(-world_size / 2, world_size / 2, image_size, endpoint=True)
+    grid = np.linspace(-world_size / 2, world_size / 2, 1000, endpoint=True)
 
     # Randomize fractal noise parameters
-    restart = np.random.randint(low=world_size // 3, high=world_size // 2)
+    restart = np.random.randint(world_size // 3, world_size // 2)
     num_octaves = int(np.random.randint(*num_octaves))
     H = np.random.uniform(*H)
     lacunarity = np.random.uniform(*lacunarity)
-    offset = np.random.randint(low=0, high=world_size // 2)
+    offset = np.random.randint(0.0, world_size // 2)
 
-    # Vectorized fractal noise calculation
+    # Generate the height map using fractal noise
     d_map = []
     for x in grid:
         d_row = []
@@ -112,15 +111,17 @@ def _generate_height_map(
             d_row.append(z)
         d_map.append(d_row)
 
-    # Convert the result to a numpy array
-    height_map = np.array(d_map)
+    # Resize the height map to the desired image size
+    d_map = np.array(d_map)
+    d_map = Image.fromarray(d_map).resize((image_size,) * 2, Image.Resampling.BILINEAR)
+    d_map = np.array(d_map)
 
-    # Normalize the height map to [0, 1]
-    height_min = np.min(height_map)
-    height_max = np.max(height_map)
-    normalized_height_map = (height_map - height_min) / (height_max - height_min)
+    # Normalize the height map to [0, 255]
+    d_min = np.min(d_map)
+    d_max = np.max(d_map)
+    normalized_height_map = (d_map - d_min) / (d_max - d_min) * 255
 
-    return _generate_segmentation_map(normalized_height_map, band=band)
+    return normalized_height_map
 
 
 def _generate_segmentation_map(
@@ -139,19 +140,14 @@ def _generate_segmentation_map(
         seg_map: A 3-channel segmentation map (RGB).
         masks: Tuple of binary masks (grass, non-grass, beds).
     """
-    np.random.seed(42)
-
-    # Scale the height map to [0, 255]
-    scaled_height_map = height_map * 255
-
-    # Precompute threshold values
+    # Threshold values
     lower_band = 128 - band
     upper_band = 128 + band
 
     # Create binary masks for grass, not grass, and beds as uint8 arrays
-    grass = ((scaled_height_map > lower_band) & (scaled_height_map < upper_band)).astype(np.uint8) * 255
-    not_grass = (scaled_height_map >= upper_band).astype(np.uint8) * 255
-    beds = (scaled_height_map <= lower_band).astype(np.uint8) * 255
+    grass = ((height_map > lower_band) & (height_map < upper_band)).astype(np.uint8) * 255
+    not_grass = (height_map >= upper_band).astype(np.uint8) * 255
+    beds = (height_map <= lower_band).astype(np.uint8) * 255
 
     # Create segmentation map with 3 channels (R: not_grass, G: grass, B: beds) by stacking the channels
     seg_map = np.zeros((height_map.shape[0], height_map.shape[1], 3), dtype=np.uint8)
@@ -160,6 +156,6 @@ def _generate_segmentation_map(
     seg_map[..., 2] = beds
 
     # Normalize the scaled height map to [0, 1]
-    scaled_height_map /= 255
+    height_map /= 255
 
-    return scaled_height_map, seg_map, (grass, not_grass, beds)
+    return height_map, seg_map, (grass, not_grass, beds)
