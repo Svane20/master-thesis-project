c81e1a8b03a75294adfc5869e096b65ab87e6fd8
Kasper Svane <svane.kasper20@gmail.com>
2025-02-13 22:24:22 +0100

    Fixed issue on Windows for the playground directory

--- synthetic-data-generation/utils/utils.py
+++ synthetic-data-generation/utils/utils.py
@@ -60,7 +60,7 @@ def get_playground_directory_with_tag(configuration: Configuration) -> Path:
     Returns:
         Path: The path to the playground directory.
     """
-    current_time = datetime.now(timezone.utc).strftime("%H:%M_%d-%m-%Y")
+    current_time = datetime.now(timezone.utc).strftime("%H_%M_%d-%m-%Y")
     directory = Path(configuration.directories.playground_directory) / current_time
     create_directory(directory)
 
