eae0d42b8f6371b71ff6cb30e6188d28a359c026
svane20 <svane.kasper20@gmail.com>
2025-02-27 12:53:37 +0000

    ml/optimization
    Working on optimizations for training the ML model to predict alpha matting masks

--- machine-learning/unet/datasets/synthetic/data_loaders.py
+++ machine-learning/unet/datasets/synthetic/data_loaders.py
@@ -1,40 +1,26 @@
 from torch.utils.data import DataLoader, Dataset
-
 import albumentations as A
 from pathlib import Path
 from typing import Tuple
-
 from datasets.synthetic.synthetic_dataset import SyntheticDataset
 from datasets.transforms import get_train_transforms, get_val_transforms
-
 from configuration.dataset import DatasetConfig
 from configuration.scratch import ScratchConfig
-
 
 def setup_data_loaders(
         scratch_config: ScratchConfig,
         dataset_config: DatasetConfig
 ) -> Tuple[DataLoader[Dataset], DataLoader[Dataset]]:
-    """
-    Set up the data loaders for training.
-
-    Args:
-        scratch_config (ScratchConfig): Configuration for scratch.
-        dataset_config (DatasetConfig): Configuration for the dataset.
-
-    Returns:
-        Tuple[DataLoader[Dataset], DataLoader[Dataset]]: Train and validation data loaders.
-    """
-    # Get train and test transforms
+    # Get train and validation transforms.
     transforms = get_train_transforms(scratch_config.resolution)
     target_transforms = get_val_transforms(scratch_config.resolution)
 
-    # Construct data filepaths
+    # Construct data filepaths.
     current_directory = Path(__file__).resolve().parent.parent.parent
     train_directory = current_directory / dataset_config.root / dataset_config.name / "train"
     val_directory = current_directory / dataset_config.root / dataset_config.name / "val"
 
-    # Create the data loaders
+    # Create the data loaders.
     train_data_loader = create_data_loader(
         directory=train_directory,
         transforms=transforms,
@@ -56,7 +42,6 @@ def setup_data_loaders(
 
     return train_data_loader, val_data_loader
 
-
 def create_data_loader(
         directory: Path,
         transforms: A.Compose,
@@ -66,21 +51,6 @@ def create_data_loader(
         shuffle: bool,
         drop_last: bool,
 ) -> DataLoader[Dataset]:
-    """
-    Create a data loader for the training data.
-
-    Args:
-        directory (Path): Path to the training data directory.
-        batch_size (int): Batch size for the data loader
-        num_workers (int): Number of workers to use for data loading.
-        shuffle (bool): Whether to shuffle the data.
-        pin_memory (bool): Whether to pin memory for faster data loading.
-        drop_last (bool): Whether to drop the last incomplete batch.
-        transforms (albumentations.Compose): Transforms to apply to the image data.
-
-    Returns:
-        DataLoader[Dataset]: Data loader for the training data
-    """
     dataset = SyntheticDataset(
         image_directory=directory / "images",
         mask_directory=directory / "masks",
